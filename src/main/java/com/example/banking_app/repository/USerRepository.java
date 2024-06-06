b6b1f9346dfc6371ac9bca0eb5a37b26bb248599
Akloo-jamilu <jhasleg126@yahoo.com>
2024-06-06 12:58:10 +0100

    structures

---
+++ src/main/java/com/example/banking_app/repository/USerRepository.java
@@ -0,0 +1,8 @@
+package com.example.banking_app.repository;
+
+import com.example.banking_app.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface USerRepository  extends JpaRepository<User, Long> {
+
+}
