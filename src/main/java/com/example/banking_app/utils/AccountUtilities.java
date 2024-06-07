d60b12e97ec71fe8633ac60046181ae0a6288143
Akloo-jamilu <jhasleg126@yahoo.com>
2024-06-07 14:59:54 +0100

    helper method for account generation

--- src/main/java/com/example/banking_app/utils/AccountUtilities.java
+++ src/main/java/com/example/banking_app/utils/AccountUtilities.java
@@ -5,20 +5,22 @@ import java.util.HashSet;
 
 public class AccountUtilities {
 
-   public String generateAccountNumber(){
+   public static String generateAccountNumber(){
 
-       Year year = Year.now();
+       Year PresentYear = Year.now();
         int min = 100000; // Smallest 6-digit number
        int max = 999999; // Largest 6-digit number
 
 
        int randNumber = (int) Math.floor(Math.random() * (max - min + 1) + min);
 
-       String yearString = String.valueOf(year);
+       String yearString = String.valueOf(PresentYear);
        String randNumberString = String.valueOf(randNumber);
 
        StringBuilder accountNumber = new StringBuilder();
+       accountNumber.append(PresentYear).append(randNumber).toString();
 
+       return yearString + randNumberString;
    }
 }
 
