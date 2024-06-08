7f67e247aa1977a88b76dbe13d96ea80cb7fd813
Akloo-jamilu <jhasleg126@yahoo.com>
2024-06-08 12:43:17 +0100

    mail services

---
+++ src/main/java/com/example/banking_app/dto/EmailDetails.java
@@ -0,0 +1,9 @@
+package com.example.banking_app.dto;
+
+public class EmailDetails {
+    private String recipient;
+    private String messageBody;
+    private String subject;
+    private String attachment;
+
+}
