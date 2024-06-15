fa862fb26eaf0a103593e5b5df0683a8283a54c4
Akloo-jamilu <jhasleg126@yahoo.com>
2024-06-15 22:18:22 +0100

    adding swagger dependency

--- src/main/java/com/example/banking_app/BankingCoreApplication.java
+++ src/main/java/com/example/banking_app/BankingCoreApplication.java
@@ -1,9 +1,34 @@
 package com.example.banking_app;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Banking Application",
+				description = "App REST APIs",
+				version = "v1.00",
+				contact = @Contact(
+						name = "Akloo",
+						email = "contactAkloo",
+						url = "Null"
+				),
+				license = @License(
+						name = "Banking Application",
+						url = "Null"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Banking APIs",
+				url = "Null"
+		)
+)
 public class BankingCoreApplication {
 
 	public static void main(String[] args) {
