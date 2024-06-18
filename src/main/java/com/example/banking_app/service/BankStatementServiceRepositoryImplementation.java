bb223b06840fb77052c156c55aad1c7f1fb5537c
Akloo-jamilu <jhasleg126@yahoo.com>
2024-06-18 23:07:55 +0100

    generating bank statement

--- src/main/java/com/example/banking_app/service/BankStatementServiceRepositoryImplementation.java
+++ src/main/java/com/example/banking_app/service/BankStatementServiceRepositoryImplementation.java
@@ -1,11 +1,18 @@
 package com.example.banking_app.service;
 
+import com.example.banking_app.entity.Transaction;
 import com.example.banking_app.repository.TransactionRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @AllArgsConstructor
 public class BankStatementServiceRepositoryImplementation {
-    private TransactionRepository transactionRepository
+    private TransactionRepository transactionRepository;
+
+    public List<Transaction> generateStatement(String accountNumber, String startDate, String endDate ){
+        List<Transaction> transactions = transactionRepository.findAll().stream().filter(transaction -> transaction.getAccountNumber().equals(accountNumber));
+    }
 }
