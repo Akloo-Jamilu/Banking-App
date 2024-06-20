03a56a1a95a565a2c58000831648b56cea2cd29f
Akloo-jamilu <jhasleg126@yahoo.com>
2024-06-20 18:08:08 +0100

    implimenting statement ui using itext pdf

--- src/main/java/com/example/banking_app/service/BankStatementServiceRepositoryImplementation.java
+++ src/main/java/com/example/banking_app/service/BankStatementServiceRepositoryImplementation.java
@@ -10,6 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.time.LocalDate;
@@ -25,24 +26,15 @@ public class BankStatementServiceRepositoryImplementation {
     private TransactionRepository transactionRepository;
     private static final String FILE = "C:\\Users\\Admin\\Document\\MyStatement.pdf";
 
-    public List<Transaction> generateStatement(String accountNumber, String startDate, String endDate ){
+    public List<Transaction> generateStatement(String accountNumber, String startDate, String endDate ) throws DocumentException, FileNotFoundException {
         // Trim the date strings to remove any leading or trailing whitespace
         LocalDate startingFrom = LocalDate.parse(startDate.trim(), DateTimeFormatter.ISO_DATE);
         LocalDate endingIn = LocalDate.parse(endDate.trim(), DateTimeFormatter.ISO_DATE);
         LocalDateTime startOfDay = startingFrom.atStartOfDay();
         LocalDateTime endOfDay = endingIn.plusDays(1).atStartOfDay().minusSeconds(1);
 
-        return transactionRepository.findAll().stream()
-//                .filter(transaction -> transaction.getAccountNumber().equals(accountNumber))
-//                .filter(transaction -> transaction.getCreatedAt().isEqual(startOfDay)).
-//                filter(transaction -> transaction.getCreatedAt().isEqual(endOfDay)).toList();
 
-                .filter(transaction -> transaction.getAccountNumber().equals(accountNumber))
-                .filter(transaction -> !transaction.getCreatedAt().isBefore(startOfDay) && !transaction.getCreatedAt().isAfter(endOfDay))
-                .toList();
-    }
 
-    private void designStatement(List<Transaction> transactions){
         Rectangle rectangle = new Rectangle(PageSize.A4);
         Document document = new Document(rectangle);
         log.info("setting paper size");
@@ -58,9 +50,24 @@ public class BankStatementServiceRepositoryImplementation {
 
         PdfPCell bankAddress = new PdfPCell(new Phrase("No. 89, Aguyi Iro-sin Boulevard"));
         bankAddress.setBorder(0);
+        bankHeadingTable.addCell(bankName);
+        bankHeadingTable.addCell(bankAddress);
+
+        PdfPTable statementInfoSection = new PdfPTable(2);
+        PdfPCell customerInfo = new PdfPCell( new Phrase("Start" +" "+startOfDay ))
 
 
+        return transactionRepository.findAll().stream()
+//                .filter(transaction -> transaction.getAccountNumber().equals(accountNumber))
+//                .filter(transaction -> transaction.getCreatedAt().isEqual(startOfDay)).
+//                filter(transaction -> transaction.getCreatedAt().isEqual(endOfDay)).toList();
+
+                .filter(transaction -> transaction.getAccountNumber().equals(accountNumber))
+                .filter(transaction -> !transaction.getCreatedAt().isBefore(startOfDay) && !transaction.getCreatedAt().isAfter(endOfDay))
+                .toList();
     }
+
+
 
 
 }
