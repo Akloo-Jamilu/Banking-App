2de3d772737726dc6180f1cb366d506b976d311e
Akloo-jamilu <jhasleg126@yahoo.com>
2024-06-25 21:52:51 +0100

    implimenting statement ui using itext pdf

--- src/main/java/com/example/banking_app/service/BankStatementServiceRepositoryImplementation.java
+++ src/main/java/com/example/banking_app/service/BankStatementServiceRepositoryImplementation.java
@@ -117,6 +117,8 @@ public class BankStatementServiceRepositoryImplementation {
 
 //        document.close();//        document.close();document.close();
         //        document.close();//        document.close();document.close();
+        //        document.close();//        document.close();document.close();
+        //        document.close();//        document.close();document.close();
         document.close();
 
 
